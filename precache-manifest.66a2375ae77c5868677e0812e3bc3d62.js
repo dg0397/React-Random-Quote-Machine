d1a921018a549cc80d99a6f820184022aa38cafb
dg0397 <rgg0397@gmail.com>
2020-06-23 00:43:21 -0400

    Updates

---
+++ precache-manifest.66a2375ae77c5868677e0812e3bc3d62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63e3afb3681fb5bc5250d5bf36d8b2f",
+    "url": "/React-Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "fe610829a55b1c1ea135",
+    "url": "/React-Random-Quote-Machine/static/css/main.1945033f.chunk.css"
+  },
+  {
+    "revision": "4c794144120b52261d77",
+    "url": "/React-Random-Quote-Machine/static/js/2.04e42080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Random-Quote-Machine/static/js/2.04e42080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe610829a55b1c1ea135",
+    "url": "/React-Random-Quote-Machine/static/js/main.d10ad41b.chunk.js"
+  },
+  {
+    "revision": "d883eb6a19cdcfb39d13",
+    "url": "/React-Random-Quote-Machine/static/js/runtime-main.a0e5e117.js"
+  }
+]);
